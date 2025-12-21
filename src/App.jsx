e9547d3aef274cda3d132125229c70bcf107b1dc
joara <143992699+joara-publisher@users.noreply.github.com>
2025-12-22 08:47:21 +0900

    [feat] - button 컴포넌트 공용 사용 가능하도록 구현 (#3)
    * [feat] - Header 컴포넌트 생성, 페이지별 조건 구현
    
    * [feat] - button 컴포넌트 공용 사용 가능하도록 구현

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,17 @@
 import { BrowserRouter, Navigate, Route, Routes } from 'react-router-dom';
 import Layout from './Layout';
 import List from './pages/List';
+import GlobalStyle from './styles/GlobalStyle';
 
 function Main() {
   
   return (
     <BrowserRouter>
+      <GlobalStyle />
       <Routes>
         <Route element={<Layout />}>
-          <Route index element={<List />} />
-          <Route path="/list" element={<Navigate to="/" replace />} />
+          <Route index element={<Navigate to="/list" replace />}  />
+          <Route path="/list" element={<List />} />
         </Route>
       </Routes>
     </BrowserRouter>
