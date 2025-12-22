9cde206a9abd733c48b5c5b20e4f65b32ac17b09
Hoon Oh <shoonoh11@gmail.com>
2025-12-22 09:39:50 +0900

    feat: 기가막힌 상수 만들기!

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,17 @@
-import { BrowserRouter, Navigate, Route, Routes } from 'react-router-dom';
-import Layout from './Layout';
-import List from './pages/List';
-import GlobalStyle from './styles/GlobalStyle';
+import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
+import Layout from "./Layout";
+import List from "./pages/List";
+import GlobalStyle from "./styles/GlobalStyle";
 
 function Main() {
-  
+  const constantValue = 42;
+
   return (
     <BrowserRouter>
       <GlobalStyle />
       <Routes>
         <Route element={<Layout />}>
-          <Route index element={<Navigate to="/list" replace />}  />
+          <Route index element={<Navigate to="/list" replace />} />
           <Route path="/list" element={<List />} />
         </Route>
       </Routes>
