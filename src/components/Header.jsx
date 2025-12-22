d65ad1b64cfaff765d4ef4ae9de44c7d0be4aa6c
joara <143992699+joara-publisher@users.noreply.github.com>
2025-12-23 08:53:50 +0900

    [refactor] 공통 Button 컴포넌트 props 네이밍 및 사용성 개선 (#7)

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,26 +1,28 @@
 import { Link, useLocation, useNavigate } from "react-router-dom";
 import Button from "./Button";
 
-function Header () {
+function Header() {
   const location = useLocation();
   const navigate = useNavigate();
-  
+
   return (
     <header>
       <div>
         <Link to="/list">
           <img src="" alt="링크샵 로고" />
         </Link>
-        {location.pathname === '/list' ? 
-          <>
-            <Button onClick={() => navigate('/linkpost')} flexLayoutType="inline-flex">생성하기</Button> 
-          </>
-          : 
-          <Button onClick={() => navigate('/list')} flexLayoutType="inline-flex">돌아가기</Button>
-        }
+        {location.pathname === "/list" ? (
+          <Button onClick={() => navigate("/linkpost")} layout="fit">
+            생성하기
+          </Button>
+        ) : (
+          <Button onClick={() => navigate("/list")} layout="fit">
+            돌아가기
+          </Button>
+        )}
       </div>
     </header>
-  )
+  );
 }
 
 export default Header;
