5d6182092457d970f478085877cccdbb66eb9c81
joara-thrlaw <arjoe207@gmail.com>
2025-12-22 18:06:21 +0900

    [refactor] 링크샵 리스트 페이지 - 멘토님 피드백 반영

--- src/components/SortOptionsButton.jsx
+++ src/components/SortOptionsButton.jsx
@@ -1,34 +1,39 @@
 import { useState } from "react";
-import { SORTING_LABEL_MAP, SORTING_OPTIONS } from "../constants/sortingOptions";
+import {
+  SORTING_LABEL_MAP,
+  SORTING_OPTIONS,
+} from "../constants/sortingOptions";
 import Modal from "../components/Modal";
 
-function FilterButton ({ sortBy, onSortChange }) {  
+function SortOptionsButton({ selectedValue, onValueChange }) {
   const [isFilterModalOpen, setIsFilterModalOpen] = useState(false);
   const [hasTouched, setHasTouched] = useState(false);
- 
+
   const handleSortSelect = (value) => {
     if (!hasTouched) {
-    	setHasTouched(true);
+      setHasTouched(true);
     }
-    onSortChange(value);
+    onValueChange(value);
     setIsFilterModalOpen(false);
-  }
-  
-  const toggleFilterModal = () => {
-    setIsFilterModalOpen(prev => !prev);
-  }
-  
+  };
+
+  const toggleSortOptionsModal = () => {
+    setIsFilterModalOpen((prev) => !prev);
+  };
+
   return (
     <>
-      <button onClick={toggleFilterModal}>{hasTouched ? SORTING_LABEL_MAP[sortBy] : '상세필터'}</button>
+      <button onClick={toggleSortOptionsModal}>
+        {hasTouched ? SORTING_LABEL_MAP[selectedValue] : "상세필터"}
+      </button>
       <Modal isOpen={isFilterModalOpen}>
         <h3>정렬</h3>
-        <button onClick={toggleFilterModal}>X</button>
+        <button onClick={toggleSortOptionsModal}>X</button>
         <ul>
           {SORTING_OPTIONS.map(({ label, value }) => (
             <li
               key={value}
-              className={sortBy === value ? "active" : ""}
+              className={selectedValue === value ? "active" : ""}
               onClick={() => handleSortSelect(value)}
             >
               {label}
@@ -37,7 +42,7 @@ function FilterButton ({ sortBy, onSortChange }) {
         </ul>
       </Modal>
     </>
-  )
+  );
 }
 
-export default FilterButton;
+export default SortOptionsButton;
--- src/pages/ShopList.jsx
+++ src/pages/ShopList.jsx
@@ -1,30 +1,33 @@
 import { useState } from "react";
 import SearchInput from "../components/SearchInput";
-import FilterButton from "../components/FilterButton";
+import SortOptionsButton from "../components/SortOptionsButton";
 import LinkCardList from "../components/LinkCardList";
 import { SORTING_OPTIONS } from "../constants/sortingOptions";
 
-function ShopList () {
-  const [searchText, setSearchText] = useState('');
+function ShopList() {
+  const [searchText, setSearchText] = useState("");
   const [sortBy, setSortBy] = useState(SORTING_OPTIONS[0].value);
 
   // 검색창에 입력 시
-  const handleKeywordChange = (e) => {
+  const handleSearchTextChange = (e) => {
     setSearchText(e.target.value);
-  }
-  
+  };
+
   // 필터 선택 시
   const handleSortByChange = (sortBy) => {
     setSortBy(sortBy);
-  }
-  
+  };
+
   return (
     <>
-      <SearchInput onChange={handleKeywordChange} />
-      <FilterButton sortBy={sortBy} onSortChange={handleSortByChange} />
-      <LinkCardList searchText={searchText} sortBy={sortBy}/>
+      <SearchInput onChange={handleSearchTextChange} />
+      <SortOptionsButton
+        selectedValue={sortBy}
+        onValueChange={handleSortByChange}
+      />
+      <LinkCardList searchText={searchText} sortBy={sortBy} />
     </>
-  )
+  );
 }
 
 export default ShopList;
