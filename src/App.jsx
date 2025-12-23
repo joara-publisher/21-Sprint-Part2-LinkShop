72c035ab4c011393d9b026ae45a3c26c28bcf5bf
YeomSeunghyeon <100lion67@gmail.com>
2025-12-23 15:11:33 +0900

    검색결과 없음 화면

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,20 @@
-
-import { BrowserRouter, Navigate, Route, Routes } from 'react-router-dom';
-import Layout from './Layout';
-import ShopList from './pages/ShopList';
-import GlobalStyle from './styles/GlobalStyle';
+import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
+import Layout from "./Layout";
+import ShopList from "./pages/ShopList";
+import GlobalStyle from "./styles/GlobalStyle";
 
 function Main() {
-  
   return (
     <BrowserRouter>
       <GlobalStyle />
       <Routes>
         <Route element={<Layout />}>
-          <Route index element={<Navigate to="/list" replace />}  />
+          <Route index element={<Navigate to="/list" replace />} />
           <Route path="/list" element={<ShopList />} />
         </Route>
       </Routes>
     </BrowserRouter>
   );
 }
-
 
 export default Main;
--- src/components/LinkCardList.jsx
+++ src/components/LinkCardList.jsx
@@ -1,46 +1,56 @@
-import useShopList from "../hooks/useShopList"
-import { ShopGrid,ShopCard ,ShopItem,ProductItem,ShopText,EmptyResult,EmptyResultText} from "../styles/LinkCardList.styles";
-import SearchNull from "../../public/img/img_search_null.png"
-const LinkCardList=({searchText,sortBy})=>{
-  const list=useShopList({
+import useShopList from "../hooks/useShopList";
+import {
+  ShopGrid,
+  ShopCard,
+  ShopItem,
+  ProductItem,
+  ShopText,
+  EmptyResult,
+  EmptyResultText,
+} from "../styles/LinkCardList.styles";
+import SearchNull from "../../public/img/img_search_null.png";
+const LinkCardList = ({ searchText, sortBy }) => {
+  const list = useShopList({
     keyword: searchText,
     orderBy: sortBy,
   });
   const hasKeyword = searchText.trim().length > 0;
-  return(
-    hasKeyword&&list.length==0?
+  return hasKeyword && list.length == 0 ? (
     <EmptyResult>
       <img src={SearchNull} alt="검색 결과 없음" width="375px" height="182px" />
-      <EmptyResultText>검색 결과가 없어요 <br/>지금 프로필을 만들고 내 상품을 소개해보세요</EmptyResultText>
-    </EmptyResult>:
-    <ShopGrid >
-        {list&&list.map((item)=>{
-          return(
-          <ShopCard key={item.id}>
-          <ShopItem>
-         <img src={item.shop.imageUrl}  width={30}/>
-          <ShopText>
-          <div>{item.name}</div>
-          <div>@{item.userId}</div>
-          </ShopText>
-          <div>{item.likes}</div>
-          </ShopItem>
-          <div>대표상품 {item.productsCount}</div>
-          <ProductItem>
-          {item.products.map((product)=>{
-            return(
-           <div>
-             <img src={product.imageUrl}  width={30}/>
-           </div>
-            )
-          })}
-          </ProductItem>
-      
-          </ShopCard>
-          )
+      <EmptyResultText>
+        검색 결과가 없어요 <br />
+        지금 프로필을 만들고 내 상품을 소개해보세요
+      </EmptyResultText>
+    </EmptyResult>
+  ) : (
+    <ShopGrid>
+      {list &&
+        list.map((item) => {
+          return (
+            <ShopCard key={item.id}>
+              <ShopItem>
+                <img src={item.shop.imageUrl} width={30} />
+                <ShopText>
+                  <div>{item.name}</div>
+                  <div>@{item.userId}</div>
+                </ShopText>
+                <div>{item.likes}</div>
+              </ShopItem>
+              <div>대표상품 {item.productsCount}</div>
+              <ProductItem>
+                {item.products.map((product) => {
+                  return (
+                    <div>
+                      <img src={product.imageUrl} width={30} />
+                    </div>
+                  );
+                })}
+              </ProductItem>
+            </ShopCard>
+          );
         })}
     </ShopGrid>
-  )
-
+  );
 };
 export default LinkCardList;
