f9b218328d26fe21805f81e19552ed5b0124def5
YeomSeunghyeon <100lion67@gmail.com>
2025-12-23 14:36:24 +0900

    검색결과 없음 화면

--- src/styles/LinkCardList.styles.js
+++ src/styles/LinkCardList.styles.js
@@ -26,3 +26,17 @@ export const ShopText=styled.div`
   display:flex;
    flex-direction: column;
 `
+export const EmptyResult=styled.div`
+  display:flex;
+   flex-direction: column;
+  align-items:center;
+`
+export const EmptyResultText=styled.div`
+font-size:17px;
+font-weight:500;
+line-height: 100%;
+margin-top:32px;
+text-align:center;
+line-height: 100%;
+letter-spacing: 0;
+`
