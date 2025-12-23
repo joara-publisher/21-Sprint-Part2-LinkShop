9aab124b1257732754b33275e15c98e2eaaf9bad
joara-thrlaw <arjoe207@gmail.com>
2025-12-23 14:35:49 +0900

    [feat] 상세 필터 버튼 스타일링

---
+++ src/styles/SortOptionsButtonStyles.js
@@ -0,0 +1,28 @@
+import styled from "styled-components";
+import filterIcon from "../assets/icon_sort_button.svg";
+
+export const SortButton = styled.button`
+  position: relative;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  gap: 4px;
+  font-weight: 500;
+  font-size: 18px;
+  color: var(--black);
+  padding: 0;
+  margin: 0;
+  background-color: var(--white_FFFFFF);
+  border: none;
+  cursor: pointer;
+
+  &:after {
+    content: "";
+    width: 12px;
+    height: 12px;
+    background-image: url("${filterIcon}");
+    background-repeat: no-repeat;
+    background-size: 7px 6px;
+    background-position: center calc(50% + 1px);
+  }
+`;
