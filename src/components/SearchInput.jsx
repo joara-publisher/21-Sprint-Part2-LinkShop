f91b2117c8030d27bafe97a3a3a4236140c4eca6
chungman <leecm940603@gmail.com>
2025-12-24 18:18:21 +0900

    [feat] - Form 컴포넌트 파일 입력 상태 관리 및 이미지 파일 미리보기 로직 추가

--- src/components/SearchInput.jsx
+++
@@ -1,9 +0,0 @@
-function SearchInput({ onChange }) {
-  return (
-    <div>
-      <input type="text" placeholder="샵 이름으로 검색해 보세요." onChange={onChange} />
-    </div>
-  )
-}
-
-export default SearchInput;
