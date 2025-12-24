3add40c80061e198ca7a381784ec5a80998ebfbc
YeomSeunghyeon <100lion67@gmail.com>
2025-12-24 10:24:27 +0900

    검색결과 없음 화면

--- src/hooks/useShopList.js
+++ src/hooks/useShopList.js
@@ -1,10 +1,8 @@
 import { useEffect, useState } from "react";
 import { getProducts } from "../utils/product.api";
-
 const useShopList = ({ keyword, orderBy } = {}) => {
   const [shopList, setShopList] = useState([]);
   const [nextCursor, setNextCursor] = useState(0);
-
   const fetchProducts = async () => {
     try {
       const res = await getProducts({ keyword, orderBy, cursor: nextCursor });
@@ -19,12 +17,9 @@ const useShopList = ({ keyword, orderBy } = {}) => {
       console.log("상품 목록 조회 에러:", error);
     }
   };
-
   useEffect(() => {
     fetchProducts();
   }, [keyword, orderBy]);
-
   return { shopList, nextCursor, fetchProducts };
 };
-
 export default useShopList;
